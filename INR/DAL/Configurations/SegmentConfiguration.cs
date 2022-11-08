eade105eecb05df61706af5ff3f647c5c77921a1
sadidkhan <sadid.edu@gmail.com>
2022-11-08 15:11:03 -0800

    Segment seed data updated

--- INR/DAL/Configurations/SegmentConfiguration.cs
+++ INR/DAL/Configurations/SegmentConfiguration.cs
@@ -14,8 +14,12 @@ namespace INR.DAL.Configurations
 
             builder.HasData(new Segment { Id = 1, Name = "IPT" },
                 new Segment { Id = 2, Name = "ET" },
-                new Segment { Id = 3, Name = "MTR" },
-                new Segment { Id = 4, Name = "PR" });
+                new Segment { Id = 3, Name = "MTR1" },
+                new Segment { Id = 4, Name = "PR" },
+                new Segment { Id = 5, Name = "MTR2" },
+                new Segment { Id = 6, Name = "TG" },
+                new Segment { Id = 7, Name = "GI" },
+                new Segment { Id = 8, Name = "GP" });
         }
     }
 }
